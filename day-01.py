5901653196c568aecec98040157c968d3a8c603f
Ben Schomp <ben@benschomp.com>
2023-12-01 10:23:05 -0500

    part 2 not working (must sub left to right, NOT one to nine)

--- day-01.py
+++ day-01.py
@@ -1,25 +1,42 @@
 
 file = open('day-01.txt', 'r')
 
-answer = 0
+def getTerm( word ):
+  result = 0
+  for c in word:
+    if c.isdigit():
+      result += int(c)*10
+      break
+
+  for c in reversed( word ):
+    if c.isdigit():
+      result += int(c)
+      break
+
+  return result
+
+def convertLine( word ):
+  word = word.replace( "one", "1" )
+  word = word.replace( "two", "2" )
+  word = word.replace( "three", "3" )
+  word = word.replace( "four", "4" )
+  word = word.replace( "five", "5" )
+  word = word.replace( "six", "6" )
+  word = word.replace( "seven", "7" )
+  word = word.replace( "eight", "8" )
+  word = word.replace( "nine", "9" )
+  print( word )
+  return word
+
+
+
+part_one = part_two = 0
 for line in file:
   line = line.strip()
-
-  term = 0
-  for c in line:
-    if c.isdigit():
-      term += int(c)*10
-      break
-
-  for c in reversed(line):
-    if c.isdigit():
-      term += int(c)
-      break
-
-  answer += term
-  print( line, term )
+  part_one += getTerm( line )
+  part_two += getTerm( convertLine( line ) )
 
 file.close()
 
-print( answer )
-
+print( 'part one:', part_one )
+print( 'part two:', part_two )

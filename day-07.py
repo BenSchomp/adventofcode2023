3c6a5f32407460310fb0d3aebcc41a10e994b3d5
Ben Schomp <ben@benschomp.com>
2023-12-07 18:47:50 -0500

    day 7, part 1 reduced and better, ready for part 2

--- day-07.py
+++ day-07.py
@@ -1,23 +1,17 @@
 
-#cardRank = {'2':2, '3':3, '4':4, '5': 5, '6':6, '7':7, '8':8, '9':9, \
-#            'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
 cardRank = {'2':2, '3':3, '4':4, '5': 5, '6':6, '7':7, '8':8, '9':9, \
-            'T':10, 'J':1, 'Q':12, 'K':13, 'A':14}
-rankDisplay = {10:'T', 11:'J', 12:'Q', 13:'K', 14:'A'}
-
+            'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+#cardRank = {'2':2, '3':3, '4':4, '5': 5, '6':6, '7':7, '8':8, '9':9, \
+#            'T':10, 'J':1, 'Q':12, 'K':13, 'A':14}
 
 class Hand:
   def __init__(self, cards, wager):
     self.cards = cards
-    self.hiGroup = None
-    self.hiCount = 0
-    self.loGroup = None
-    self.loCount = 0
-    self.kickers = []
-
+    self.counts = {}
+    self.score = 0
     self.wager = int(wager)
 
-    self.rankHand()
+    self.scoreHand()
 
   def __str__(self):
     return self.getDisplayHand()
@@ -26,10 +20,8 @@ class Hand:
     return sorted(self.cards) == sorted(other.cards)
 
   def __lt__(self, other):
-    selfRank = (self.hiCount * 10) + self.loCount
-    otherRank = (other.hiCount * 10) + other.loCount
-    if selfRank != otherRank:
-      return selfRank < otherRank
+    if self.score != other.score:
+      return self.score < other.score
 
     for i in range(5):
       a = cardRank[self.cards[i]]
@@ -39,73 +31,19 @@ class Hand:
 
     return False
 
-  def getRankDisplay(self, card):
-    if card > 9:
-      return rankDisplay[card]
-    else:
-      return str(card)
-
-  def getDisplayHand(self):
-    result = ''
-    if self.hiGroup:
-      c = self.getRankDisplay(self.hiGroup)
-      for i in range(self.hiCount):
-        result += c
-    if self.loGroup:
-      c = self.getRankDisplay(self.loGroup)
-      for i in range(self.loCount):
-        result += c
-    for c in self.kickers:
-      result += self.getRankDisplay(c)
-    return result
-
-  def rankHand(self):
-    jCount = 0
+  def scoreHand(self):
     for i in range(5):
-      count = 1
       cur = cardRank[self.cards[i]]
-      if not cur in self.kickers:
-        self.kickers.append(cur)
+      if cur in self.counts:
+        self.counts[cur] += 1
+      else:
+        self.counts[cur] = 1
 
-      if self.cards[i] == 'J':
-        jCount += 1
-
-      for j in range(5):
-        if i == j:
-          continue
-
-        if self.cards[i] == self.cards[j]:
-          count += 1
-
-      if count > 1:
-        if count > self.hiCount:
-          if cur != self.hiGroup:
-            self.loGroup = self.hiGroup
-            self.loCount = self.hiCount
-            self.hiGroup = cur
-          self.hiCount = count
-        elif count == self.hiCount and cur != self.hiGroup:
-          self.loCount = count
-          if cur > self.hiGroup:
-            self.loGroup = self.hiGroup
-            self.hiGroup = cur
-          else:
-            self.loGroup = cur
-        elif cur != self.hiGroup:
-          if count > self.loCount:
-            self.loCount = count
-            self.loGroup = cur
-          elif count == self.loCount:
-            if cur > self.loGroup:
-              self.loGroup = cur
-
-      if self.hiCount > 1:
-        self.hiCount += jCount
-
-    for i in [self.hiGroup, self.loGroup]:
-      if i in self.kickers:
-        self.kickers.remove(i)
-    self.kickers.sort(reverse=True)
+    scores = sorted(self.counts.values(), reverse=True)
+    self.score += scores[0]*10
+    if len(scores) > 1:
+      self.score += scores[1]
+     
 
 # --- main --- #
 
@@ -122,7 +60,6 @@ file.close()
 
 part_one = 0
 for i, h in enumerate(sorted(hands)):
-  print( i+1, h.getDisplayHand(), h.wager, h.cards )
   part_one += (i+1) * h.wager
 
 print( "part_one:", part_one )

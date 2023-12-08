617891bc666de4673a20ce3e88d595a021714c88
Ben Schomp <ben@benschomp.com>
2023-12-08 01:44:34 -0500

    day 8, part 2 works for example, but brute force isn't cutting it

--- day-08.py
+++ day-08.py
@@ -12,22 +12,65 @@ for line in file:
 
 file.close()
 
-#print( instructions, nodes )
+# --- one --- #
+def one():
+  done = False
+  count = 0
+  curNode = 'AAA'
+  while not done:
+    for i in instructions:
+      count += 1
+      if i == 'L':
+        curNode = nodes[curNode][0]
+      else:
+        curNode = nodes[curNode][1]
+      #print( curNode )
+      if curNode == 'ZZZ':
+        done = True
+        break
 
-done = False
-count = 0
-curNode = 'AAA'
-while not done:
-  for i in instructions:
-    count += 1
-    if i == 'L':
-      curNode = nodes[curNode][0]
-    else:
-      curNode = nodes[curNode][1]
-    #print( curNode )
-    if curNode == 'ZZZ':
+  print( "part_one:", count )
+
+# --- two --- #
+def two():
+  ghosts = []
+
+  for k, v in nodes.items():
+    if k[2] == 'A':
+      ghosts.append(k)
+
+  print( ghosts )
+
+  done = False
+  count = 0
+  max_x = 0
+  while not done:
+    for d in instructions:
+      dir = 0
+      if d == 'R':
+        dir = 1
+
+      count += 1
+
       done = True
-      break
+      x = 0
+      for i, curNode in enumerate(ghosts):
+        newNode = nodes[curNode][dir]
+        #print( newNode )
+        if newNode[2] != 'Z':
+          done = False
+        else:
+          x += 1
 
-print( "part_one:", count )
+        ghosts[i] = newNode
 
+      if x > max_x:
+        max_x = x
+        print( ghosts, x, max_x, count )
+
+      if done:
+        break
+
+  print( "part_two:", count )
+
+two()

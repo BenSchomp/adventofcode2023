77f73e84fb5be37350ab186835958baad47de0ad
Ben Schomp <ben@benschomp.com>
2023-12-07 22:25:30 -0500

    day 6, clean up

--- day-06.py
+++ day-06.py
@@ -10,11 +10,7 @@ times = list(map(lambda x: int(x), parts[1].split()))
 parts = line2[1].split(':')
 dists = list(map(lambda x: int(x), parts[1].split()))
 
-# --- part one --- #
-part_one = 1
-
-for i, time in enumerate(times):
-  goal = dists[i] 
+def getGoalCount(time, goal):
   count = 0
   t = 1
   speed = 1
@@ -25,26 +21,25 @@ for i, time in enumerate(times):
       count += 1
     t += 1
 
+  return count
+
+# --- part one --- #
+part_one = 1
+
+for i, time in enumerate(times):
+  goal = dists[i] 
+  count = getGoalCount(time, goal)
   part_one *= count
 
 print( "part_one:", part_one )
 
-# --- part two ---
-part_two = 1
-
 time = int(''.join(map(lambda x: str(x), times)))
 goal = int(''.join(map(lambda x: str(x), dists)))
 
-count = 0
-t = 1
-speed = 1
-while t < time:
-  speed = t
-  distance = (time - t) * speed
-  if distance > goal:
-    count += 1
-  t += 1
+# --- part two ---
+part_two = 1
 
+count = getGoalCount(time, goal)
 part_two *= count
 
 print( "part_two:", part_two )

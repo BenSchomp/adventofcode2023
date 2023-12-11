2136644f3376e7aeb46be64c5e07b6125b98dc9b
Ben Schomp <ben@benschomp.com>
2023-12-11 06:47:53 -0500

    day 11, part 2 done

--- day-11.py
+++ day-11.py
@@ -3,60 +3,70 @@ def display(grid):
   for row in grid:
     print( row )
 
-h = w = 0
 grid = []
-galaxy_columns = set()
+galaxy_cols = set()
+empty_rows = set()
 
 file = open("day-11.txt", 'r')
+row = 0
 for line in file:
   grid.append(line.strip())
 
   if not '#' in line:
-    grid.append(line.strip())
+    empty_rows.add(row)
   else:
-    for i, c in enumerate(line):
+    for col, c in enumerate(line):
       if c == '#':
-        galaxy_columns.add(i)
+        galaxy_cols.add(col)
+
+  row += 1
 
 file.close()
 
-height = len(grid)
-width = len(grid[0])
+empty_cols = set()
+for i in range(len(grid[0])):
+  if not i in galaxy_cols:
+    empty_cols.add( i )
 
-empty_columns = set()
-for i in range(width):
-  if not i in galaxy_columns:
-    empty_columns.add( i )
+def expand(expansion):
+  global grid, empty_cols, empty_rows
+  galaxies = []
 
-offset = 0
-for i in range(width):
-  if i in empty_columns:
-    for j in range(height):
-      grid[j] = grid[j][:i+offset] + '.' + grid[j][i+offset:]
-    offset += 1
+  er = sorted(empty_rows)
+  erl = len(empty_rows) # empty rows length
 
-#display( grid )
+  ec = sorted(empty_cols)
+  ecl = len(empty_cols) # empty cols length
 
-galaxies = []
-for i, row in enumerate(grid):
-  for j, c in enumerate(row):
-    if c == '#':
-      galaxies.append((i,j))
+  eri = row_offset = 0
+  for i, row in enumerate(grid):
+    if eri < erl and i >= er[eri]:
+      eri += 1
+      row_offset += expansion-1
 
-#print( galaxies )
+    eci = col_offset = 0
+    for j, c in enumerate(row):
+      if eci < ecl and j >= ec[eci]:
+        eci += 1
+        col_offset += expansion-1
+      if c == '#':
+        galaxies.append((j+col_offset,i+row_offset))
 
-part_one = 0
-numGalaxies = len(galaxies)
-for i in range(numGalaxies):
-  for j in range(numGalaxies):
-    if i == j:
-      continue
-    g1 = galaxies[i]
-    g2 = galaxies[j]
-    dist = abs(g2[0] - g1[0]) + abs(g2[1] - g1[1])
-    part_one += dist
+  result = 0
+  numGalaxies = len(galaxies)
+  for i in range(numGalaxies):
+    for j in range(numGalaxies):
+      if i == j:
+        continue
+      g1 = galaxies[i]
+      g2 = galaxies[j]
+      dist = abs(g2[0] - g1[0]) + abs(g2[1] - g1[1])
+      result += dist
 
-print( "part_one:", int(part_one/2) )
+  return int(result/2)
+
+print( "part_one:", expand(2) )
+print( "part_two:", expand(1000000) )
 
 
 

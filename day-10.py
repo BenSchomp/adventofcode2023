24babcb60058222e6272cdaf80d31583d395efa2
Ben Schomp <ben@benschomp.com>
2023-12-12 08:13:18 -0500

    day 10, part 2 done

--- day-10.py
+++ day-10.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(100000)
 
 def getKey(x,y):
   return "%d,%d" % (x,y)
@@ -52,146 +54,156 @@ class Node:
     self.neighbors.append(getKey(tryX, tryY))
 
 def solve(x, y):
-  global width, height, maze, visited
-  if x == 0 and y == 0:
+  global width, height
+  global maze, visited
+  k = getKey(x,y)
+  if maze[y][x] == 0 and (x == 0 or x == width-1 or y == 0 or y == height-1):
     return True
-  if maze[y][x] == 1 or visited[y][x] == 1:
+  if maze[y][x] == 1 or k in visited:
     return False
 
-  visited[y][x] = 1
+  visited.add(k)
 
-  if x != 0:
-    if solve(x-1, y):
-      return True
-  if x != width-1:
-    if solve(x+1, y):
-      return True
-  if y != 0:
-    if solve(x, y-1):
-      return True
-  if y != height-1:
-    if solve(x, y+1):
-      return True
+  if solve(x-1, y):
+    return True
+  if solve(x+1, y):
+    return True
+  if solve(x, y-1):
+    return True
+  if solve(x, y+1):
+    return True
 
   return False
 
+max_x = max_y = None
+widgh = height = None
+maze = []
+visited = set()
 
 # --- main ---
+def main():
 
-row = -1 
-col = -1 
-nodes = {}
-max_x = None
-max_y = None
+  row = -1 
+  col = -1 
+  nodes = {}
+  global max_x, max_y, width, height
+  global maze, visited
 
-file = open("day-10.txt", 'r')
-lines = []
-max_y = -1 
-for line in file:
-  max_y += 1
-  lines.append(line.strip())
-file.close()
-max_x = len(lines[0])-1
+  file = open("day-10.txt", 'r')
+  lines = []
+  max_y = -1 
+  for line in file:
+    max_y += 1
+    lines.append(line.strip())
+  file.close()
+  max_x = len(lines[0])-1
 
-startX = startY = None
-for line in lines:
-  row += 1
-  for c in line.strip():
-    col += 1
-    if c == '.':
-      continue
+  startX = startY = None
+  for line in lines:
+    row += 1
+    for c in line.strip():
+      col += 1
+      if c == '.':
+        continue
+      else:
+        n = Node(col, row, c)
+        nodes[n.key] = n
+        if c == 'S':
+          startX = col
+          startY = row
+    col = -1
+
+  # --- part one --- #
+
+  N = 0
+  E = 1
+  S = 2
+  W = 3
+
+  # find any direction that connects to Start
+  curX = startX
+  curY = startY
+  curKey = startKey = getKey(curX, curY)
+  for d in range(4):
+    tryX = curX
+    tryY = curY
+
+    if d == N:
+      tryY -= 1
+    elif d == E:
+      tryX += 1
+    elif d == S:
+      tryY += 1
     else:
-      n = Node(col, row, c)
-      nodes[n.key] = n
-      if c == 'S':
-        startX = col
-        startY = row
-  col = -1
+      tryX -= 1
 
-# --- part one --- #
-
-N = 0
-E = 1
-S = 2
-W = 3
-
-# find any direction that connects to Start
-curX = startX
-curY = startY
-curKey = startKey = getKey(curX, curY)
-for d in range(4):
-  tryX = curX
-  tryY = curY
-
-  if d == N:
-    tryY -= 1
-  elif d == E:
-    tryX += 1
-  elif d == S:
-    tryY += 1
-  else:
-    tryX -= 1
-
-  tryKey = getKey(tryX, tryY)
-  if tryKey in nodes and curKey in nodes[tryKey].neighbors:
-    curKey = tryKey
-    break
-
-path = []
-path.append(curKey)
-curNode = nodes[curKey]
-while curNode.shape != 'S':
-  for tryKey in curNode.neighbors:
-    if tryKey == startKey and len(path) ==1:
-      continue
-
-    if tryKey not in path:
+    tryKey = getKey(tryX, tryY)
+    if tryKey in nodes and curKey in nodes[tryKey].neighbors:
       curKey = tryKey
-      path.append(curKey)
-      curNode = nodes[curKey]
       break
 
-part_one = int(len(path)/2)
-print( "part_one:", part_one )
+  path = []
+  path.append(curKey)
+  curNode = nodes[curKey]
+  while curNode.shape != 'S':
+    for tryKey in curNode.neighbors:
+      if tryKey == startKey and len(path) ==1:
+        continue
 
-# --- part two --- #
+      if tryKey not in path:
+        curKey = tryKey
+        path.append(curKey)
+        curNode = nodes[curKey]
+        break
 
-# make the map double sized so can use maze solving to find
-#  cells that are inner vs outer (doubling allows travel through
-#  adjacent pipes more obvious)
-# also add a border of open space to allow any outer open space a
-#  path to reach (0,0)
-width = (max_x+1)*2 + 2
-height = (max_y+1)*2 + 2
+  part_one = int(len(path)/2)
+  print( "part_one:", part_one )
 
-maze = [[ 0 for x in range(width)] for y in range(height)]
-visited = [[ 0 for x in range(width)] for y in range(height)]
+  # --- part two --- #
 
-# translate to the double map
-for y in range(max_y):
-  for x in range(max_x):
-    curKey = getKey(x, y)
-    if curKey in path:
-      maze[(y*2)+1][(x*2)+1] = 1
+  # make the map double sized so can use maze solving to find
+  #  cells that are inner vs outer (doubling allows travel through
+  #  adjacent pipes more obvious)
+  # also add a border of open space to allow any outer open space a
+  #  path to reach (0,0)
+  width = (max_x+1)*2
+  height = (max_y+1)*2
 
-# now just need to connect the pipes along the path to fill in the gaps
+  maze = [[ 0 for x in range(width)] for y in range(height)]
 
-curX = (startX*2)+1
-curY = (startY*2)+1
-for p in path:
-  (tgtX, tgtY) = p.split(',')
-  tgtX = (int(tgtX)*2)+1
-  tgtY = (int(tgtY)*2)+1
-  gapX = int((curX+tgtX)/2)
-  gapY = int((curY+tgtY)/2)
-  maze[gapY][gapX] = 1
+  # translate to the double map
+  for y in range(max_y+1):
+    for x in range(max_x+1):
+      curKey = getKey(x, y)
+      if curKey in path:
+        maze[y*2][x*2] = 1
 
-  curX = tgtX
-  curY = tgtY
+  # now just need to connect the pipes along the path to fill in the gaps
+  curX = startX*2
+  curY = startY*2
+  for p in path:
+    (tgtX, tgtY) = p.split(',')
+    tgtX = int(tgtX)*2
+    tgtY = int(tgtY)*2
+    gapX = int((curX+tgtX)/2)
+    gapY = int((curY+tgtY)/2)
+    maze[gapY][gapX] = 1
 
-tryX = 8
-tryY = 8 
-print( solve(tryX, tryY) )
-display(maze)
+    curX = tgtX
+    curY = tgtY
 
+  # lastly, try every spot not in the path
+  part_two = 0
+  for tryX in range(max_x+1):
+    for tryY in range(max_y+1):
+      if getKey(tryX, tryY) in path:
+        continue
 
+      visited.clear()
+      if not solve(tryX*2, tryY*2):
+        part_two += 1
+
+  print( "part_two:", part_two )
+
+if __name__=="__main__":
+  main()

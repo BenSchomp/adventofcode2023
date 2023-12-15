15b849bda01218d742a3eefef3e4cc2d71e678c1
Ben Schomp <ben@benschomp.com>
2023-12-15 16:26:13 -0500

    day 13, part 2 done

--- day-13.py
+++ day-13.py
@@ -6,20 +6,15 @@ H = 0
 V = 1
 
 def isMirror(line, i):
-  #print( 'isMirror', line, i )
   j = i+1
   while i>=0 and j<len(line):
-    #print( i, j, line[i], line[j] )
     if line[i] != line[j]:
-      #print( 'nope' )
       return False
     i -= 1
     j += 1
-  #print( 'yep:', )
   return True
 
 def getMirrorIndexes(line):
-  #print( 'getMirrorIndexes', line )
   result = []
   i = 0
   while i < len(line)-1:
@@ -28,51 +23,45 @@ def getMirrorIndexes(line):
     i += 1
   return result
 
-def getMirrorScore(grid):
+def getMirrorScore(grid, scoreToIgnore=None):
   # instead, find a mirror in row 1 ... then try all other rows at that col only
   # if all rows find a mirror at same col, that's the mirror col
   # if not, continue searching across row 1
-
-  hIndexes = getMirrorIndexes(grid[0])
-  #print( hIndexes )
+  score = None
+  vIndexes = getMirrorIndexes(grid[0])
 
   found = None
-  for i in hIndexes:
+  for i in vIndexes:
     found = [H, i]
     for line in grid[1:]:
       if not isMirror(line, i):
-        #print( ' ! not a mirror !' )
         found = None
         break
 
     if found:
-      break
+      score = found[1]+1
+      if score != scoreToIgnore:
+        return score
 
-  if found:
-    #print( 'found one!', found )
-    return found[1]+1
-
-  vIndexes = getMirrorIndexes( [g[0] for g in grid] )
+  hIndexes = getMirrorIndexes( ''.join([g[0] for g in grid]) )
 
   found = None
-  for i in vIndexes:
+  for i in hIndexes:
     found = [V, i]
     col = 1 
     while col < len(grid[0]):
-      line = [g[col] for g in grid]
+      line = ''.join([g[col] for g in grid])
       if not isMirror(line, i):
         found = None
         break
       col += 1
 
     if found:
-      break
+      score = (found[1]+1) * 100
+      if score != scoreToIgnore:
+        return score
 
-  #if found:
-    #print( 'found one!', found)
-
-  return (found[1]+1) * 100
-
+  return None
 
 
 # --- main --- #
@@ -92,13 +81,33 @@ grid = []
 
 file.close()
 
-# --- part one --- #
-part_one = 0
+part_one = part_two = 0
 for grid in grids:
-  tmp = getMirrorScore( grid )
-  #print( 'mirror score:',tmp )
-  part_one += tmp
+  # --- part one --- #
+  origScore = getMirrorScore( grid )
+  part_one += origScore
+
+  # --- part two --- #
+  done = False
+  for r, row in enumerate(grid):
+    if done:
+      break
+    for c, ch in enumerate(row):
+      newGrid = grid[:]
+      newRow = row[:]
+      if ch == '.':
+        newRow = row[:c] + '#' + row[c+1:]
+      else:
+        newRow = row[:c] + '.' + row[c+1:]
+      newGrid[r] = newRow
+
+      newScore = getMirrorScore( newGrid, origScore )
+      if newScore:
+        part_two += newScore
+        done = True
+        break
 
 print( "part_one:", part_one )
+print( "part_two:", part_two )
 
 
